0a84040352fbb64b1a5776d445553a649aa53a35
evgen <57286862+evgen-chern@users.noreply.github.com>
2021-10-06 13:43:25 +0300

    3.4

--- js/script.js
+++ js/script.js
@@ -87,44 +87,53 @@ testWebP(function (support) {
      });
 
 
+     /*BURGER DROP*/
+     $(".header-menu .title_svg").click(function(){
+             $(this).toggleClass("active")
+             $(this).siblings(".menu__links").slideToggle().css("display", "flex")
+     })
 
-     const animItems = document.querySelectorAll('.anim_item');
-     if (animItems.length >0){
-         window.addEventListener('scroll', animOnScroll);
-         function animOnScroll(){
-             for (let index = 0; index < animItems.length; index++) {
-                 const animItem = animItems[index];
-                 const animItemHeight = animItem.offsetHeight;
-                 const animItemOffset = offset(animItem).top;
-                 const animStart = 4;
 
-                 let animItemPoint = window.innerHeight - animItemHeight / animStart;
+     if(window.innerWidth >= 780){
+         const animItems = document.querySelectorAll('.anim_item');
+         if (animItems.length >0){
+             window.addEventListener('scroll', animOnScroll);
+             function animOnScroll(){
+                 for (let index = 0; index < animItems.length; index++) {
+                     const animItem = animItems[index];
+                     const animItemHeight = animItem.offsetHeight;
+                     const animItemOffset = offset(animItem).top;
+                     const animStart = 4;
 
-                 if(animItemHeight > window.innerHeight) {
-                     animItemPoint = window.innerHeight - window.innerHeight / animStart;
-                 }
+                     let animItemPoint = window.innerHeight - animItemHeight / animStart;
 
-                 if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-                     animItem.classList.add('anim');
-                 } else {
-                     if(!animItem.classList.contains('no_anim')){
-                         animItem.classList.remove('anim');
+                     if(animItemHeight > window.innerHeight) {
+                         animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                     }
+
+                     if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                         animItem.classList.add('anim');
+                     } else {
+                         if(!animItem.classList.contains('no_anim')){
+                             animItem.classList.remove('anim');
+                         }
                      }
                  }
              }
-         }
-         function offset(el) {
-             const rect = el.getBoundingClientRect(),
-                 scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-                 scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-             return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
-         }
+             function offset(el) {
+                 const rect = el.getBoundingClientRect(),
+                     scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+                 return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+             }
 
-         setTimeout(() => {
-             animOnScroll();
-         }, 400);
+             setTimeout(() => {
+                 animOnScroll();
+             }, 400);
 
+         }
      }
+
 
 
 
